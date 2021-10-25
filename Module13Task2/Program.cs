55edbfa01b75588ca23a77ce66f39ed16bcd3e45
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-10-25 23:38:15 +0600

    Добавьте файлы проекта.

---
+++ Module13Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module13Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\YOGA\Downloads\Text1.txt";
+
+            var dict = new Dictionary<long, string>();
+
+            var list = new List<string>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                var text = sr.Read();
+                var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+                foreach (var word in noPunctuationText)
+                {
+                    list.Add(word);
+                }
+            }
+        }
+    }
+}
---
+++ Module13Tasks1-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Module13Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\YOGA\Downloads\Text1.txt";
+
+            var list = new List<char>();
+
+            var linkedList = new LinkedList<char>();
+
+            var text = File.ReadAllText(path);
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var stopwatch1 = new Stopwatch();
+            stopwatch1.Start();
+            foreach (var line in noPunctuationText)
+                list.Add(line);
+            stopwatch1.Stop();
+            Console.WriteLine(stopwatch1.ElapsedMilliseconds);
+
+            //foreach(var line in list)
+            //    Console.WriteLine(line);
+
+            var stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            foreach (var line in noPunctuationText)
+                linkedList.AddLast(line);
+            stopwatch2.Stop();
+            Console.WriteLine(stopwatch2.ElapsedMilliseconds);
+
+        }
+    }
+}
